21a307ea5c90077f3a55173795b5d83475fcb2f8
jonathan2406 <jonathan2406.garcia@gmail.com>
2024-04-12 15:04:31 -0500

    un poco mas de expliacion en el ejercicio de conte

--- quiz/ejercicioConteo.py
+++ quiz/ejercicioConteo.py
@@ -24,5 +24,5 @@ class Operaciones:
             return Operaciones.factorial(n)/denominador 
     
 
-print("decidi que era una permutacion con repeticion ya que si importa el orden, si entran todos los elementos y tambien hay elementos repetidos")
+print("decidi que era una permutacion con repeticion ya que si importa el orden, si entran todos los elementos y tambien hay elementos repetidos \nn seria 10 osea la suma de todos los elementos involucrados y ya los de repeticion son los que se nombran en el enunciado")
 print(f"la solucion seria esta: {Operaciones.permutacionRepeticion(10,[5,3,2])}")
